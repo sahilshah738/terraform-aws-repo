0f6320660237b4070e67b370df6e074994c1b58f
sahilshah738 <103265684+sahilshah738@users.noreply.github.com>
2022-11-24 15:44:02 +0530

    Update c2-variables.tf

--- c2-variables.tf
+++ c2-variables.tf
@@ -9,7 +9,7 @@ variable "aws_region" {
 variable "my_s3_bucket" {
   description = "S3 Bucket name that we pass to S3 Custom Module"
   type = string
-  default = "mybucket-1047"
+  default = "sahil-terraform-bucket-24-11-2022"
 }
 
 ## Create Variable for S3 Bucket Tags
